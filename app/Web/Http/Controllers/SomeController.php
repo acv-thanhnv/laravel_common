e05a9390d703b41f46d7066a7dc44156385a6344
thanhnv-acv <thanhnv@atmarkcafe.org>
2018-06-26 17:32:48 +0700

    Inspection Code

--- app/Web/Http/Controllers/SomeController.php
+++ app/Web/Http/Controllers/SomeController.php
@@ -7,7 +7,7 @@ class SomeController extends Controller
 {
     public function index(){
         echo '<pre>';
-        \App\Core\Dao\SDB::execSPsToDataResultCollection('dsdsd');
+        \App\Core\Dao\SDB::execSPsToDataResultCollection('TEST ERROR SP');
         echo 'web/some/index';
     }
 }
